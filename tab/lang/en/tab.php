5ae45ae114a2e37846f38107b41426a789681332
Patrick Thibaudeau <patrick.thibaudeau@ualberta.ca>
2011-04-07 22:15:48 -0600

    Added all files and folders for moodle 2 tab display

---
+++ tab/lang/en/tab.php
@@ -0,0 +1,29 @@
+<?php // $Id: tab.php,v 1.2 2008/07/01 09:44:05 moodler Exp $ 
+      // tab.php - created with Moodle 1.9
+
+
+$string['ajouter'] = 'Add a new tab display';
+$string['addtab'] = 'Add a new tab';
+$string['changestyle'] = 'Modify stylesheet';
+$string['css'] = 'Stylesheet';
+$string['displaymenu'] = 'Display tab menu';
+$string['displaymenuagree'] = 'Check if you would like to display menu';
+$string['format'] = 'Tab formatting';
+$string['menucss'] = 'Modify menu stylesheet';
+$string['menuname'] = 'Menu name';
+$string['modulename'] = 'Tab display';
+$string['modulenameplural'] = 'Tab displays';
+$string['moretabs'] = 'Use more tabs';
+$string['name'] = 'Name';
+$string['noformating'] = 'No formatting';
+$string['order'] = 'The order you would like this tab';
+$string['pluginname'] = 'Tab display';
+$string['pluginadministration'] = 'Tab Administration';
+$string['tab'] = 'Tab';
+$string['tabadministration'] = 'Tab Administration';
+$string['tabcontent'] = 'Tab content';
+$string['tabname'] = 'Tab name';
+$string['taborder'] = 'Tab display activity order within menu';
+$string['updatethis'] = 'Update this tab display';
+
+?>
