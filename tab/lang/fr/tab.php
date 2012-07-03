a662d7d39e7aed1074d42807e315ed99bd7f7610
Luiggi Sansonetti <luiggi.sansonetti@univ-paris3.fr>
2012-07-03 16:13:34 +0300

    Update master

--- tab/lang/fr/tab.php
+++ tab/lang/fr/tab.php
@@ -1,12 +1,22 @@
 ﻿<?php // $Id: tab.php,v 1.2 2008/07/01 09:44:05 moodler Exp $ 
       // tab.php - created with Moodle 1.9
 $string['ajouter'] = 'Ajouter un onglet';
-$string['addtab'] = 'Àjouter un onglet';
+$string['addtab'] = 'Ajouter un onglet';
 $string['changestyle'] = 'Modifier la feuille de style';
-$string['css'] = 'feuille de style';
-$string['displaymenu'] = 'Utiliser le menu d\'onglet disponibles';
+$string['clicktoopen'] = 'Cliquer pour ouvrir';
+$string['content_help'] = 'Vous pouvez incorporer un fichier pdf en suivant ces instructions:
+<br> <ul>
+<li> tapez le nom que vous souhaitez utiliser pour le lien ; </ li>
+<li> s&eacute;lectionnez le texte et cliquez sur le bouton de lien insertion / &eacute;dition ; </ li>
+<li> cliquez sur le bouton Parcourir &agrave; la fin du champ URL du lien ;</ li>
+<li> t&eacute;l&eacute;chargez votre fichier pdf puis cliquez sur Ins&eacute;rer </ li>
+</ ul> <br>
+NE RIEN ajouter dans l'&eacute;diteur. <br>
+Enregistrer et afficher. Le fichier pdf sera int&eacute;gr&eacute;
+$string['css'] = 'Feuille de style';
+$string['displaymenu'] = 'Utiliser le menu d\'onglets';
 $string['displaymenuagree'] = 'Cochez si vous désirez utiliser le menu';
-$string['format'] = 'Formattage d\'onglet';
+$string['format'] = 'Formatage d\'onglet';
 $string['menucss'] = 'Modifier la feuille de style du menu';
 $string['menuname'] = 'Nom du menu';
 $string['modulename'] = 'Onglet';
@@ -14,13 +24,14 @@ $string['modulenameplural'] = 'Onglets';
 $string['moretabs'] = 'Utiliser plus d\'onglet';
 $string['name'] = 'Nom';
 $string['noformating'] = 'Aucun formatage';
-$string['order'] = 'L\'ordre de cette onglet';
+$string['order'] = 'L\'ordre de cet onglet';
 $string['pluginname'] = 'Onglet';
 $string['tab'] = 'Onglet';
 $string['tabadministration'] = 'Administration des onglets';
 $string['tabcontent'] = 'Content de l\'onglet';
 $string['tabname'] = 'Nom de l\'onglet';
 $string['taborder'] = 'Ordre des onglet dans le menu';
+$string['tab:view'] = 'Vue des onglets';
 $string['updatethis'] = 'Update this tab display';
 
 ?>
